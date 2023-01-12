a8fb2df347d802e322d4f8a949e0f54a48243a87
paulabf <115157928+paulabf1@users.noreply.github.com>
2023-01-12 14:43:40 +0100

    Add files via upload

---
+++ ejercicio7.js
@@ -0,0 +1,54 @@
+//Ejercicio 5
+var j =0;
+while(j < frutas_y_verduras.length){
+    console.log(frutas_y_verduras[j]);
+    j = j +1;
+}
+
+//Ejercicio 6
+//crea una función que reciba una lista
+// como parámetro y devuelva el número de elementos
+// de la lista
+
+function totalElementoslista(lista){
+    return lista.length;
+
+}
+
+// ejercicio 7
+//crea una función que reciba una lista
+// y un elemento como parámeytos y devuelva
+//los elementos de la lista falso y verdadero
+// parámetros= lista
+
+function seEncuentraElementosEnLista(lista, elemento){
+    for (var i=0; i<lista.length; i++){
+        if(lista[i] == elemento){
+            return true;
+
+        }
+    }
+     return false;
+}
+
+//Ejercicio 8
+//Crea una función que reciba una lista
+// y un elemento como parámetros
+//y elimine el elemento de la lista
+//si se encuentra en ella.
+
+function borrarDeLista(lista, elemento){
+
+}
+
+//Ejercicio 9
+//Crea una función que reciba una lista de números de la lista
+// de números como parámetros y devuelva
+//el mayor número de la lista.
+
+function mayordeLista(lista){
+    for (var i=0; i>lista.sort; i++)
+}
+
+
+//Crea una función que reciba una lista de números 
